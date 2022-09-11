3bd453ee475f19e5148323fdc64aae38ff72d15a
Camila Stefano <camilavictoriastefano@gmail.com>
2022-09-10 22:15:49 -0300

    feat(contactForm) add form validations

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+
+// Styles
 import "./styles/variables.css";
 import "./styles/styles.css";
+import "./styles/utils.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,27 +1,50 @@
 import "./styles.css";
+import { useForm } from "react-hook-form";
 
 const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
   return (
     <section className="contact">
       <h1 className="contact__title">You can use the following form to reach out to us</h1>
 
-      <form className="contact__form" method="post" action="mailto:camilavictoriastefano@gmail.com">
+      <form className="contact__form" onSubmit={handleSubmit(onSubmit)}>
         <div className="contact__form__input">
-          <label for="uname">Name:</label>
-          <input type="text" id="uname" name="uname" required autofocus />
-          <p className="input-error"></p>
+          <label htmlFor="uname">Name:</label>
+          <input type="text" id="uname" name="uname" autoFocus {...register("uname", { required: true, maxLength: 20 })} />
+
+          {errors.uname && <p className="error">This field is required</p>}
         </div>
+
         <div className="contact__form__input">
-          <label for="email">Email:</label>
-          <input type="email" id="email" size="30" minlength="3" maxlength="64" required />
-          <p className="input-error"></p>
+          <label htmlFor="email">Email:</label>
+          <input
+            id="email"
+            size="30"
+            {...register("email", { required: true, pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ })}
+          />
+
+          {errors.email?.type === "required" && <p className="error">This field is required</p>}
+          {errors.email?.type === "pattern" && <p className="error">Email address invalid</p>}
         </div>
         <div className="contact__form__textarea">
-          <textarea id="msg" name="message" rows="10" minlength="10" maxlength="1500" required placeholder="Write your message here..."></textarea>
-          <p className="input-error"></p>
+          <textarea
+            id="msg"
+            name="message"
+            rows="10"
+            placeholder="Write your message here..."
+            {...register("message", { required: true, minLength: 20 })}
+          />
+
+          {errors.message?.type === "required" && <p className="error">This field is required</p>}
+          {errors.message?.type === "minLength" && <p className="error">Minimum 20 characters required</p>}
         </div>
-        <button className="preview-btn btn-form">Preview your message</button>
-        <input className="btn-form submit" type="submit" value="Submit" />
+        <button className="preview-btn btn-form">Submit</button>
       </form>
       <section className="preview"></section>
     </section>
